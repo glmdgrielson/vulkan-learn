1af40e1d04506b80a980631861f7302c669f698b
glmdgrielson <zapperchamp1@gmail.com>
2023-07-10 19:11:58 -0400

    Add triangle/main.cpp
    Doing the basic Vulkan triangle tutorial. Best of luck?

---
+++ triangle/main.cpp
@@ -0,0 +1,40 @@
+#include <vulkan/vulkan.h>
+
+#include <cstdlib>
+#include <cstdio>
+#include <stdexcept>
+
+class HelloTriangle {
+public:
+    void run() {
+        this->initVulkan();
+        this->mainLoop();
+        this->cleanup();
+    }
+
+private:
+    void initVulkan() {
+        //
+    }
+
+    void mainLoop() {
+        //
+    }
+
+    void cleanup() {
+        //
+    }
+};
+
+int main() {
+    HelloTriangle app;
+
+    try {
+        app.run();
+    } catch (std::exception& err) {
+        printf("%s\n", err.what());
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
