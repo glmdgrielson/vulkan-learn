66c6dc2ea81968769fbc7c959c17377fbb086e3d
glmdgrielson <zapperchamp1@gmail.com>
2023-07-10 19:34:32 -0400

    Add GLFW3 dependency
    Thanks, tutorial for springing this on me.
    Why the heck does Vulkan not handle this already?

--- triangle/main.cpp
+++ triangle/main.cpp
@@ -8,22 +8,42 @@
 class HelloTriangle {
 public:
     void run() {
+        this->initWindow();
         this->initVulkan();
         this->mainLoop();
         this->cleanup();
     }
 
 private:
+    GLFWwindow* window;
+    const uint32_t HEIGHT = 800;
+    const uint32_t WIDTH = 600;
+
+    void initWindow() {
+        // Initialize the graphics library?
+        glfwInit();
+
+        // Apparently we're not getting a window?
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        this->window = glfwCreateWindow(this->HEIGHT, this->WIDTH, "Vulkan", nullptr, nullptr);
+    }
+
     void initVulkan() {
         //
     }
 
     void mainLoop() {
-        //
+        while (!glfwWindowShouldClose(this->window)) {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() {
-        //
+        glfwDestroyWindow(this->window);
+
+        glfwTerminate();
     }
 };
 
