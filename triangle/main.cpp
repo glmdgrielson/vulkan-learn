2e2214fd84738dd854c60256f3042c9e132cf2f6
glmdgrielson <zapperchamp1@gmail.com>
2023-07-10 20:35:14 -0400

    Add Vulkan basics
    Now I'm playing with graphics!
    
    Actually, no I'm still not, because this is just pre-requisite nonsense
    before I can actually render anything of note. WELP.

--- triangle/main.cpp
+++ triangle/main.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <cstdio>
 #include <stdexcept>
+#include <cstdint>
 
 class HelloTriangle {
 public:
@@ -30,8 +31,39 @@ private:
         this->window = glfwCreateWindow(this->HEIGHT, this->WIDTH, "Vulkan", nullptr, nullptr);
     }
 
+    VkInstance instance;
+
     void initVulkan() {
         //
+    }
+
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_VERSION_1_0;
+        appInfo.pNext = nullptr; // Leaving this empty for now.
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        // We don't want extensions at the moment.
+        uint32_t glfwExtensionC = 0;
+        const char** glfwExtensionV = glfwGetRequiredInstanceExtensions(&glfwExtensionC);
+        createInfo.enabledExtensionCount = glfwExtensionC;
+        createInfo.ppEnabledExtensionNames = glfwExtensionV;
+
+        createInfo.enabledLayerCount = 0; // no idea what this means.
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &this->instance);
+
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create instance.");
+        }
     }
 
     void mainLoop() {
