57beb4eda87c3860cc3f3b5937039f463827727f
glmdgrielson <zapperchamp1@gmail.com>
2023-07-12 16:38:56 -0400

    Add debug messenger extension
    Oh boy, this API is a MESS to use. I'm really starting to miss Rust
    right now.

--- triangle/main.cpp
+++ triangle/main.cpp
@@ -25,7 +25,7 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
     switch (messageSeverity) {
 
     case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
-#ifdef VERBOSE
+#ifndef NDEBUG
         fprintf(stderr, "Validation layer (verbose): %s\n", pCallbackData->pMessage);
 #endif
     case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
@@ -42,6 +42,21 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
     }
 
     return VK_FALSE;
+}
+
+VkResult CreateDebugUtilsMessengerEXT(
+    VkInstance instance,
+    const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+    const VkAllocationCallbacks* pAllocator,
+    VkDebugUtilsMessengerEXT* pDebugMessenger
+) {
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+
+    if (func != nullptr) {
+      return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    } else {
+      return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
 }
 
 class HelloTriangle {
@@ -80,9 +95,35 @@ private:
     }
 
     VkInstance instance;
+    VkDebugUtilsMessengerEXT messenger;
 
     void initVulkan() {
         this->createInstance();
+        this->setupDebugMessenger();
+    }
+
+    void setupDebugMessenger() {
+        if (!this->enableValidationLayers) {
+            return;
+        }
+
+        VkDebugUtilsMessengerCreateInfoEXT createInfo{};
+        createInfo.sType =
+            VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        createInfo.messageSeverity =
+            VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT |
+            VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+            VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT;
+        createInfo.messageType =
+            VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+            VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT |
+            VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT;
+        createInfo.pfnUserCallback = debugCallback;
+
+        if (CreateDebugUtilsMessengerEXT(this->instance, &createInfo, nullptr,
+                                         &this->messenger) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to setup debug messenger!");
+        }
     }
 
     std::vector<const char*> getRequiredExtensions() {
